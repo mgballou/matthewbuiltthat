57f0261c74c4d81e1673faf0ffc0d787629ad578
mgballou <matthewbuiltthat@gmail.com>
2023-07-11 09:55:46 -0400

    Nav hamburger, breakpoint switching

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,12 +11,20 @@ function Header(props){
             <Link className="font-extrabold text-l py-2 px-4 transition duration-300 rounded hover:underline hover:text-blush hover:bg-gray-400/25 "to="/">matthewbuiltthat</Link>
             </div>
 
-            <div className="flex flex-row gap-4">
+            <div className="hidden lg:flex flex-row gap-4">
             <Link className="py-2 px-4 transition duration-300 rounded hover:underline hover:text-blush hover:bg-gray-400/25" to="/about">about</Link>
             <Link className="py-2 px-4 transition duration-300 rounded hover:underline hover:text-blush hover:bg-gray-400/25" to="/projects">projects</Link>
             <Link className="py-2 px-4 transition duration-300 rounded hover:underline hover:text-blush hover:bg-gray-400/25" to="/contact">contact</Link>
 
             </div>
+            <div class="lg:hidden">
+			<button class="navbar-burger flex items-center p-3">
+				<svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+					<title>Mobile menu</title>
+					<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+				</svg>
+			</button>
+		</div>
 
         </nav>
     )
